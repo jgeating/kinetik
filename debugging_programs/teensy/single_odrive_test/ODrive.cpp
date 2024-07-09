e792d1071bbf3220849af03a3b7af01353e64cbb
amorygalili <amorygalili@gmail.com>
2024-07-08 22:13:40 -0400

    Refactored odrive code. Created ODrive class with functions to set velocity and position control, set position, velocity, etc

---
+++ debugging_programs/teensy/single_odrive_test/ODrive.cpp
@@ -0,0 +1,90 @@
+#include "ODrive.h"
+
+ODrive::ODrive(const FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>& can, const int canId)
+  : m_can{ can }, m_canId{ canId } {
+  m_msg.flags.extended = false;
+}
+
+void ODrive::setAbsolutePosition(float position) {
+  m_msg.id = m_canId << 5 | 0x19;
+  m_msg.len = 4;
+  memcpy(m_msg.buf, &position, sizeof(position));
+  write();
+}
+
+void ODrive::setPosition(float position) {
+  float torqueFF = 0.0;
+
+  m_msg.id = m_canId << 5 | 0x0c;
+  m_msg.len = 8;
+  memcpy(m_msg.buf, &position, sizeof(position));
+  memcpy(m_msg.buf + sizeof(position), &torqueFF, sizeof(torqueFF));
+  write();
+}
+
+void ODrive::setVelocity(float revPerSec) {
+  float torqueFF = 0.0;
+
+  m_msg.id = m_canId << 5 | 0x0d;
+  m_msg.len = 8;
+  memcpy(m_msg.buf, &revPerSec, sizeof(revPerSec));
+  memcpy(m_msg.buf + sizeof(revPerSec), &torqueFF, sizeof(torqueFF));
+  write();
+}
+
+void ODrive::setAxisState(uint32_t state) {
+  m_msg.id = m_canId << 5 | 0x07;
+  m_msg.len = 4;
+  memcpy(m_msg.buf, &state, sizeof(state));
+  write();
+}
+
+void ODrive::disable() {
+  setAxisState(1);
+}
+
+void ODrive::enableWithClosedLoop() {
+  setAxisState(8);
+}
+
+void ODrive::enablePrintOnWrite() {
+  m_printMessageOnWrite = true;
+}
+
+void ODrive::disablePrintOnWrite() {
+  m_printMessageOnWrite = false;
+}
+
+void ODrive::printMessage() {
+  for (uint8_t i = 0; i < m_msg.len; i++) {
+    Serial.print(m_msg.buf[i], HEX);
+    if (i < m_msg.len - 1) {
+      Serial.print(", ");
+    } else {
+      Serial.println();
+    }
+  }
+}
+
+void ODrive::write() {
+  if (m_printMessageOnWrite) {
+    printMessage();
+  }
+  m_can.write(m_msg);
+}
+
+void ODrive::setControlMode(uint32_t controlMode, uint32_t inputMode) {
+  m_msg.id = m_canId << 5 | 0x0b;
+  m_msg.len = 8;
+  memcpy(m_msg.buf, &controlMode, sizeof(controlMode));
+  memcpy(m_msg.buf + sizeof(controlMode), &inputMode, sizeof(inputMode));
+  write();
+}
+
+void ODrive::setPositionControlMode() {
+  setControlMode(3, 3);
+}
+
+void ODrive::setVelocityControlMode() {
+  setControlMode(2, 2);
+}
--- debugging_programs/teensy/single_odrive_test/single_odrive_test.ino
+++ debugging_programs/teensy/single_odrive_test/single_odrive_test.ino
@@ -1,86 +1,55 @@
 #include <FlexCAN_T4.h>
+#include "ODrive.h"
 // #include <math.h>
 // #include "utils.h"
 
 // CAN related
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
 
-unsigned char steer_stmp[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-unsigned char drive_stmp[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-bool ext = false;   // Extended format flag
-int id_steer = 1;       // Motor ID 
+int id_steer = 1;       // Motor ID
 int id_drive = 0;       // Drive motor ID
-CAN_message_t steer_msg;
-CAN_message_t drive_msg;
-float pos = 0.0;  // Steering motor position
-float steer_tff = 0.0;  // Steering motor feedforward torque 
+float pos = 0.0;        // Steering motor position
+float steer_tff = 0.0;  // Steering motor feedforward torque
 
-float vel = 0.0;  // Drive motor velocity 
+float vel = 0.0;  // Drive motor velocity
 float drive_tff = 0.0;
 
+ODrive steerMotor{ Can0, id_steer };
+ODrive driveMotor{ Can0, id_drive };
+
 void setup(void) {
-  Serial.begin(115200); delay(400);
-  pinMode(6, OUTPUT); digitalWrite(6, LOW); /* optional tranceiver enable pin */
+  Serial.begin(115200);
+  delay(400);
+  pinMode(6, OUTPUT);
+  digitalWrite(6, LOW); /* optional tranceiver enable pin */
   Can0.begin();
   Can0.setBaudRate(1000000);
 
-  // Initialize ODrive 
-  steer_msg.id = id_steer << 5 | 0x19;  // reset position to 0 (if odrive has been on)
-  steer_msg.flags.extended = false;
-  // steer_msg.buf[0] = 0x00;
-  // steer_msg.buf[1] = 0x00;
-  // steer_msg.buf[2] = 0x00;
-  // steer_msg.buf[3] = 0x00;
-  steer_msg.len = 8;
-
-  Can0.write(steer_msg);
+  // Initialize ODrive
+  steerMotor.enablePrintOnWrite();
+  steerMotor.setAbsolutePosition(0);
+  driveMotor.enablePrintOnWrite();
+  driveMotor.setAbsolutePosition(0);
   delay(3000);
+  steerMotor.enableWithClosedLoop();
+  steerMotor.setVelocityControlMode();
+  driveMotor.enableWithClosedLoop();
+  driveMotor.setVelocityControlMode();
+  delay(1000);
 }
 
 void loop() {
 
   // ***** Steering *****
-  pos = pos + 0.001;
+  pos = pos - 0.001;
   Serial.print("Steering position: ");
   Serial.println(pos);
-  memcpy(steer_stmp, &pos, sizeof(pos));
-  memcpy(steer_stmp + sizeof(pos), &steer_tff, sizeof(steer_tff));
-  steer_msg.id = id_steer << 5 | 0x0c;
-  steer_msg.flags.extended = false;
-  steer_msg.len = sizeof(steer_stmp);
-  for ( uint8_t i = 0; i < 8; i++ ){
-    steer_msg.buf[i] = steer_stmp[i];
-    Serial.print(steer_msg.buf[i], HEX);
-    if (i < 7){
-      Serial.print(", ");
-    } else {
-      Serial.println();
-    }
-  } 
-  Can0.write(steer_msg);
+  // steerMotor.setPosition(pos);
+  steerMotor.setVelocity(.3);
+  delayMicroseconds(50);
+  driveMotor.setVelocity(3);
   delayMicroseconds(50);
 
-  // ***** Drive *****
-  vel = vel + .001;
-  Serial.print("drive vel: ");
-  Serial.println(vel);
-  memcpy(drive_stmp, &vel, sizeof(vel));
-  memcpy(drive_stmp + sizeof(vel), &drive_tff, sizeof(drive_stmp));
-  drive_msg.id = id_drive << 5 | 0x0c;
-  drive_msg.flags.extended = false;
-  drive_msg.len = sizeof(drive_stmp);
-  for ( uint8_t i = 0; i < 8; i++ ){
-    drive_msg.buf[i] = drive_stmp[i];
-    Serial.print(drive_msg.buf[i], HEX);
-    if (i < 7){
-      Serial.print(", ");
-    } else {
-      Serial.println();
-    }
-  } 
-  Can0.write(drive_msg);
-  delayMicroseconds(50);
-
-  // Set loop speed roughly 
+  // Set loop speed roughly
   delay(5);
 }
